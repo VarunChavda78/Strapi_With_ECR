f7ec45e23fc8705b8ab57e003436b365b56e0128
VarunChavda78 <155780677+VarunChavda78@users.noreply.github.com>
2025-07-23 19:05:20 +0530

    update ci.yml file

--- Terraform1/variables.tf
+++ Terraform1/variables.tf
@@ -6,4 +6,9 @@ variable "aws_region" {
 variable "ecr_repo_name" {
   type        = string
   description = "Name of the ECR repository"
+  default = "strapi-repo"
+}
+
+variable "image_tag" {
+  default = "latest"
 }
