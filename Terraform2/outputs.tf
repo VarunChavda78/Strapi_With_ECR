07c1d6d15cdd4d53cae40ac19b2e9707637b4d31
VarunChavda78 <155780677+VarunChavda78@users.noreply.github.com>
2025-07-24 10:22:55 +0530

    Update terraform.yml with output

--- Terraform2/outputs.tf
+++ Terraform2/outputs.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.strapi.public_ip
 }
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.strapi.repository_url
+  description = "URL of the created ECR repository"
+}
